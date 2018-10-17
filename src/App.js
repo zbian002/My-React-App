aa9533be606e90293cc754ffdf93ff13a4d9e849
Zhen <zbian002@ucr.edu>
2018-10-16 17:10:51 -0700

    Add confirm account button

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ export default class App extends Component {
     html: '',
     fetching: false,
     localUrl: null,
+    correctAccount: false,
   }
   
   fetchHTML(url) {
@@ -29,12 +30,12 @@ export default class App extends Component {
     console.log('value', value)
     
     if (value === 'https://www.airbnb.com.au/users/show/99824610') {
-      this.setState({ localUrl: 'http://localhost:3000/airbnb.html' })
+      this.setState({ localUrl: 'http://localhost:3000/airbnb.html', correctAccount: true })
       this.fetchHTML(this.state.localUrl)
     }
     
     if (value === 'https://www.ebay.com.au/usr/twiz911') {
-      this.setState({ localUrl: 'http://localhost:3000/ebay.html' })
+      this.setState({ localUrl: 'http://localhost:3000/ebay.html', correctAccount: true })
       this.fetchHTML(this.state.localUrl)
     }
   }
@@ -51,10 +52,15 @@ export default class App extends Component {
           <input type="button" value="Load account" onClick={this.handleInput}/>
         </div>
         <div>
-        <iframe src={this.state.localUrl} 
-          className='webView'
-          title="myPage"/>
+          <iframe src={this.state.localUrl} 
+            className='webView'
+            title="myPage"/>
         </div>
+        { this.state.correctAccount ?
+            <div className="confirmAccount">
+              <input type="button" value="I confirm this is my account"/>
+            </div> : null
+        }
       </div>  
     )
   }
